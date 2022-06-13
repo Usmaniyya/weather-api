31014d61214c993276410f8b2e68c6a3cd5005d0
Usman Yahaya <usmanyahaya6967@gmail.com>
2022-06-13 10:40:19 +0000

    add weather details

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var button = document.querySelector('.button');
 var input = document.querySelector('.input');
 var city = document.querySelector('#city');
+var country = document.querySelector('#country')
 var temp = document.querySelector('#temp');
+var desc = document.querySelector('#desc');
+var hum = document.querySelector('#humidity');
+var wind = document.querySelector('#wind');
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
@@ -9,13 +13,22 @@ button.addEventListener('click', (e) => {
         .then(response => response.json())
         .then(data => {
             var cityValue = data['name'];
+            var countryValue = data['sys']['country'];
             var tempValue = data['main']['temp'];
+            var descValue = data['weather'][0]['description'];
+            var humValue = data['main']['humidity'];
+            var windValue = data['wind']['speed'];
 
-
+            // descValue.capitalize();
             var celsius = Math.floor(tempValue - 273.15);
+            var kilo = Math.floor(windValue * 3.6)
 
             city.innerHTML = cityValue;
+            country.innerHTML = countryValue;
             temp.innerHTML = celsius + '&#8451';
+            desc.innerHTML = descValue;
+            hum.innerHTML = humValue + '%';
+            wind.innerHTML = kilo + 'km/h';
         })
         .catch(err => alert("Wrong city name"));
 });
